00e7435ad79550cbc8d0aaf13792909a901c9eea
Sushma Ayenampudi <148508186+sushma280@users.noreply.github.com>
2024-01-09 19:15:46 -0600

    Add files via upload

---
+++ sql code.sql
@@ -0,0 +1,10 @@
+SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
+FROM track
+JOIN album ON album.album_id = track.album_id
+JOIN artist ON artist.artist_id = album.artist_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE 'Rock'
+GROUP BY artist.artist_id
+ORDER BY number_of_songs DESC
+LIMIT 10;
+
